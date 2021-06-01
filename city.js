cdd4822422486ab3291da304d0af6599506f07c3
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-06-01 14:43:32 -0400

    added array and html template

---
+++ city.js
@@ -0,0 +1,16 @@
+let cityStates = [{
+    state: "California",
+    stateAbbr: "CA",
+    cities: ["Los Angeles", "San Francisco", "San Diego"]
+ },
+ {
+    state: "Colorado",
+    stateAbbr: "CO",
+    cities: ["Aspen", "Boulder", "Denver", "Pagosa Springs"]
+ },
+ {
+    state: "Texas",
+    stateAbbr: "TX",
+    cities: ["Austin", "Dallas", "Houston", "San Antonio"]
+ }
+];
